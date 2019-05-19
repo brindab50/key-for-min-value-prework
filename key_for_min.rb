451b7f103d9a3fa09b24b0fafd129ca81d0b78d6
brinda behal <brindab50@gmail.com>
2019-05-19 00:50:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_value = 0 
+  lowest_value = 500
   name_hash.each do |name , value|
-    if value < 
+    if value < lowest_value 
+      lowest_value = value
   end
 end
