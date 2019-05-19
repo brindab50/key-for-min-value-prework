e79060c0e009e3ab2f6dd89681b4f95244234fbf
brinda behal <brindab50@gmail.com>
2019-05-19 01:16:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,17 @@
 
 def key_for_min_value(name_hash)
-  lowest_value = 500
-  lowest_key = 0
-  name_hash.each do |name , value|
-    if value < lowest_value 
+  if name_hash == {}
+    return nil 
+end
+    lowest_value = 500
+    lowest_key = 0
+    name_hash.each do |name , value|
+    
+  if value < lowest_value 
       lowest_value = value
       lowest_key = name 
-      
-    end
+       
   end
+end
   return lowest_key
 end
