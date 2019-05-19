2e4346e389ecd1c6c74869710c1f2d8add313399
brinda behal <brindab50@gmail.com>
2019-05-19 01:07:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 require "pry"
 def key_for_min_value(name_hash)
-  binding.pry
+ 
   lowest_value = 500
   lowest_key = 0
   name_hash.each do |name , value|
+    
     if value < lowest_value 
       lowest_value = value
       lowest_key = name 
